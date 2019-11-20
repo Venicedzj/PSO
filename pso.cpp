a7802847bf905fcbb372b628f0d56fa33e8af5c8
deng <deng@546053297@qq.com>
2019-11-20 20:18:17 +0800

    improve keep best strategy

--- pso.cpp
+++ pso.cpp
@@ -5,7 +5,7 @@
 #include<iomanip>
 using namespace std;
 
-#define PARTICLE_NUM 40		//particle's number
+#define PARTICLE_NUM 20		//particle's number
 #define DIMENSION 10		//spatial dimension
 #define POS_MAX 100.0		//maximum position coordinate
 #define POS_MIN -POS_MAX	//minimum position coordinate
@@ -15,8 +15,10 @@ using namespace std;
 #define c2 2.0				//social learning coefficient
 #define wMax 0.9			//maximum weights
 #define wMin 0.4			//minimum weights
-#define Tmax 100			//maximum iterations number
+#define Tmax 2000			//maximum iterations number
 int T = 1;					//current iterations number
+#define PI acos(-1)
+#define e exp(1)
 
 /* particle structrue */
 struct Particle
@@ -37,7 +39,6 @@ struct GBest {
 /* function statement */
 void initPtc();
 double getFitnessVal(vector<double>);
-void get_gBest();
 void update_V_POS();
 void print(GBest);
 inline double random(double a, double b) { return ((double)rand() / RAND_MAX) * (b - a) + a; }
@@ -51,7 +52,6 @@ int main() {
 	initPtc();
 	while (T <= Tmax) {
 		update_V_POS();
-		get_gBest();
 		print(gBest);
 		T++;
 	}
@@ -67,8 +67,12 @@ void initPtc() {
 			ptc.POS.push_back(random(POS_MIN, POS_MAX));
 		}
 		ptc.fitness = getFitnessVal(ptc.POS);
+
+		//initialize pBest
 		ptc.pBest_pos = ptc.POS;
 		ptc.pBest_ftns = ptc.fitness;
+
+		//initialize gBest
 		if (ptc.pBest_ftns < gBest.fitness) {
 			gBest.POS = ptc.pBest_pos;
 			gBest.fitness = ptc.pBest_ftns;
@@ -92,45 +96,43 @@ void update_V_POS() {
 			if (ptc.POS[i] < POS_MIN) ptc.POS[i] = POS_MIN;
 		}
 		ptc.fitness = getFitnessVal(ptc.POS);
+
+		//keep the best
 		if (ptc.fitness < ptc.pBest_ftns) {
 			ptc.pBest_pos = ptc.POS;
 			ptc.pBest_ftns = ptc.fitness;
+			//keep gBest
+			if (ptc.pBest_ftns < gBest.fitness) {
+				gBest.POS = ptc.pBest_pos;
+				gBest.fitness = ptc.pBest_ftns;
+			}
 		}
-	}
-}
-
-/*calculate gBest after updating velocity and position*/
-void get_gBest() {
-	double minimum = particles[0].fitness;
-	int flag = 0;
-	for (int i = 0; i < PARTICLE_NUM; i++) {
-		if (particles[i].fitness < minimum) {
-			flag = i;
-			minimum = particles[i].fitness;
-		}
-	}
-	if (particles[flag].fitness < gBest.fitness) {
-		gBest.POS = particles[flag].POS;
-		gBest.fitness = particles[flag].fitness;
 	}
 }
 
 /*using position coordinates as parameter calculate the fitness value by fitness function*/
 double getFitnessVal(vector<double> POS) {
-	double fitness = 0;
-	for (auto pos : POS) {
-		fitness += pos * pos;
+	double fitness = 0.0;
+
+	///Sphere function
+	for (auto i : POS) {
+		fitness += i * i;
 	}
+
+	///Ackley function
+	//double sum1 = 0.0, sum2 = 0.0;
+	//for (auto i : POS) {
+	//	sum1 += pow(i, 2);
+	//	sum2 += cos(2 * PI * i);
+	//}
+	//fitness = 20 + e - 20 * exp(-0.2 * sqrt(sum1 / DIMENSION)) - exp(sum2 / DIMENSION);
+
 	return fitness;
 }
 
 void print(GBest gBest) {
-	cout << setiosflags(ios::left) << setw(3) 
-		<< T << " gBest fitness value: " << gBest.fitness;
-	cout << " gBest value: ";
-	for (auto i : gBest.POS) {
-		cout << setiosflags(ios::fixed) << setprecision(5) << setiosflags(ios::left) << setw(8) 
-			<< i << " ";
-	}
+	cout << setiosflags(ios::left) << T << " gBest fitness value: " << gBest.fitness;
+	//cout << " gBest value: ";
+	//for (auto i : gBest.POS) cout << setiosflags(ios::left) << i << " ";
 	cout << endl;
 }
